3d6b46b6576bca3c37f2b705d449f85ce55355ac
veizz <veizzsmile@gmail.com>
2014-12-04 01:07:36 +0800

    support ttsEngine

--- js/ttsEngine.js
+++ js/ttsEngine.js
@@ -1,15 +1,23 @@
 (function(){
-    var speakListener = function(utterance, options, sendTtsEvent) {
+    //var tts_url_prefix = "http://127.0.0.1/voice?word=";
+    var speakListener = function(utterance, options, cb) {
+        
+        cb({type:'start', charIndex:0});
         console.log(utterance);
-        console.log(options);
+        setTimeout(function(){
+            //console.log(utterance);
+            //console.log(options);
+            cb({type:'end', charIndex:utterance.length});
+            //cb({type:'error', charIndex:utterance.length , errorMessage:'eeee'});
+        }, 2000);
         //console.log(sendTtsEvent);
         //return;
-        sendTtsEvent({'event_type': 'start', 'charIndex': 0})
+        //sendTtsEvent({'event_type': 'start', 'charIndex': 0})
 
         // (start speaking)
         //console.log(utterance);
 
-        sendTtsEvent({'event_type': 'end', 'charIndex': utterance.length})
+        //sendTtsEvent({'event_type': 'end', 'charIndex': utterance.length})
     };
 
     var stopListener = function() {
@@ -19,5 +27,4 @@
 
     chrome.ttsEngine.onSpeak.addListener(speakListener);
     chrome.ttsEngine.onStop.addListener(stopListener);
-
 })();
